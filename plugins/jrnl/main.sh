06aa8eca055c88a37efffe5f6bb0cfde2cfe9a76
M. Elliot Frost <melliotfrost@gmail.com>
2026-03-04 01:41:43 -0500

    update for v6.5

--- plugins/jrnl/main.sh
+++ plugins/jrnl/main.sh
@@ -3,7 +3,7 @@ set -Eeuo pipefail
 
 if ! command -v jrnl &>/dev/null; then
   echo "installing jrnl"
-  pip install jrnl
+  pipx install jrnl
   echo "✅ jrnl succesfully installed"
 else
   echo "✅ jrnl already installed"
